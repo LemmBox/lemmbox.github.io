4dc30fecf19d03de93db44ccfe00220a1b61b07a
LemonLime <111824263+HappyLemonLime@users.noreply.github.com>
2025-03-31 01:02:15 -0700

    Added an extra message that can appear next to the mods name

---
+++ first_visit.js
@@ -0,0 +1,13 @@
+// Some stuff I was testing, it is currently not used for anything, but I am planning on implementing a tutorial of sorts for new users to BeepBox or any of the Beep mods.
+
+
+
+
+// if (localStorage.getItem("FirstVisit") === null) {
+//         console.log('1 This is a new user!')
+//         localStorage.setItem("FirstVisit", true);
+//         console.log('2 FirstVisit is set!');
+// }
+//     else {
+//         console.log('3 This is a NOT new user!');
+// }
